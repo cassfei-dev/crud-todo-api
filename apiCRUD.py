c5c47df1eac2ed1b7e7b4bd6e7b3362d4865c4e7
cassfei-dev <cassandrafaithebcas@gmail.com>
2026-07-27 19:57:23 +0800

    Stage 1: Stage 1: root and health endpoints

--- apiCRUD.py
+++ apiCRUD.py
@@ -3,5 +3,15 @@ from fastapi import FastAPI
 myApp = FastAPI()
 
 @myApp.get("/")
-def hello():
-    return{"message": "Hello World!"}
+def door():
+    return {
+        "name": "Task API",
+        "version":"1,0",
+        "endpoints": ["/tasks"]
+    }
+
+@myApp.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
